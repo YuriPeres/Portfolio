883ac828ac2c264e6f98e116b5419794755cfbfd
Yuri Peres <yuriperes92@gmail.com>
2026-05-19 00:52:30 -0300

    mudei alguma coisa antes de formatar no ProjectsList

--- src/data/ProjectsList.js
+++ src/data/ProjectsList.js
@@ -1,18 +1,22 @@
 export const projects = [
-    {
-        nome: "Gerador de Palavra",
-        thumbnail: "https://raw.githubusercontent.com/YuriPeres/word_generator/main/style/img/thumbnail.png",
-        descricao: "Gere palavras aleatórias, anagramas personalizados e combinações únicas com critérios específicos.",
-        linkGithub: "https://github.com/YuriPeres/word_generator",
-        linkDeploy: "https://word-generator-yp.netlify.app/",
-        tags: ["JavaScript", "Bootstrap", "HTML", "CSS", "WEB"],
-    },
-    {
-        nome: "Gerador de Palavra",
-        thumbnail: "https://raw.githubusercontent.com/YuriPeres/word_generator/main/style/img/thumbnail.png",
-        descricao: "Gere palavras aleatórias, anagramas personalizados e combinações únicas com critérios específicos.",
-        linkGithub: "https://github.com/YuriPeres/word_generator",
-        linkDeploy: "https://word-generator-yp.netlify.app/",
-        tags: ["JavaScript", "Bootstrap", "HTML", "CSS", "WEB"],
-    },
+  {
+    nome: "Gerador de Palavra",
+    thumbnail:
+      "https://raw.githubusercontent.com/YuriPeres/word_generator/main/style/img/thumbnail.png",
+    descricao:
+      "Gere palavras aleatórias, anagramas personalizados e combinações únicas com critérios específicos.",
+    linkGithub: "https://github.com/YuriPeres/word_generator",
+    linkDeploy: "https://word-generator-yp.netlify.app/",
+    tags: ["JavaScript", "Bootstrap", "HTML", "CSS", "WEB"],
+  },
+  {
+    nome: "Gerador de Palavra",
+    thumbnail:
+      "https://raw.githubusercontent.com/YuriPeres/word_generator/main/style/img/thumbnail.png",
+    descricao:
+      "Gere palavras aleatórias, anagramas personalizados e combinações únicas com critérios específicos.",
+    linkGithub: "https://github.com/YuriPeres/word_generator",
+    linkDeploy: "https://word-generator-yp.netlify.app/",
+    tags: ["JavaScript", "Bootstrap", "HTML", "CSS", "WEB"],
+  },
 ];
